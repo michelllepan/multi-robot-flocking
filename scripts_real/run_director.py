157d291fd8f546bbe22e3eabac2ea4ea5f6b7bf8
Michelle Pan <michellepan8@gmail.com>
2024-08-22 10:45:18 -0700

    Add command line option for robots

--- scripts_real/run_director.py
+++ scripts_real/run_director.py
@@ -1,15 +1,20 @@
+import argparse
+
 import rclpy
 
 from flocking.ros2 import Director
 
 
-def main(args=None):
+def main(args=None, robots=(1,)):
     rclpy.init(args=args)
-    director = Director(robots=(1,2))
+    director = Director(robots=robots)
     rclpy.spin(director)
     detector.destroy_node()
     rclpy.shutdown()
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--robots", "-r", nargs="+", type=int, required=True)
+    args = parser.parse_args()
+    main(robots=tuple(args.robots))
