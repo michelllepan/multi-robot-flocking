08a146b542b6f7154181b5926c1b9d1952dcf251
Michelle Pan <michellepan8@gmail.com>
2024-08-30 18:44:08 -0700

    [WIP] Add Boids to director

--- flocking/bridge/director.py
+++ flocking/bridge/director.py
@@ -1,5 +1,7 @@
+import numpy as np
 import redis
 
+from flocking.boids import BoidsRunner
 from flocking.utils import Goal, Pose
 
 GOAL_TOLERANCE = 0.1
@@ -13,40 +15,63 @@ class Director:
     def __init__(self, robots=(1,)):
         super().__init__()
         self.robots = robots
-        self.redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT)
 
+        # redis setup
+        self.redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT)
         self.goal_keys = {}
         self.pose_keys = {}
-
-        self.goals = {}
-        self.poses = {}
 
         for r in self.robots:
             self.goal_keys[r] = "robot_" + str(r) + "::goal"
             self.pose_keys[r] = "robot_" + str(r) + "::pose"
 
-        self.goals = [
-            Goal(x=0.0, y=0.0),
-            Goal(x=1.0, y=0.0),
-        ]
-        self.i = 0
+        # populate data
+        self.goals = {}
+        self.poses = {}
+        self.sync_redis()
+
+        # Boids setup
+        robot_starts = np.array([[self.poses[r].x, self.poses[r].y] for r in self.robots])
+        self.boids_runner = BoidsRunner(
+            num_robots=len(robots),
+            robot_start_positions=robot_starts,
+            step_scale=0.05,
+            canvas_dims=(4,4))
 
     def step_flocking(self):
+        # update robot positions
+        self.sync_redis()
+        robot_positions = np.array([[self.poses[r].x, self.poses[r].y] for r in self.robots])
+        self.boids_runner.move_robots(robot_positions)
+
+        # update targets
+        self.boids_runner.update_targets(mode="CIRCLE")
+        print(self.boids_runner.target_positions)
+        for i in range(len(self.robots)):
+            r = self.robots[i]
+            t = self.boids_runner.target_positions[i]
+            self.goals[r] = Goal(x=float(t[0]), y=float(t[1]))
+
+        
+    def sync_redis(self):
         for r in self.robots:
             pose_string = self.redis_client.get(self.pose_keys[r])
             if not pose_string: continue
 
             pose = Pose.from_string(pose_string)
-            if self.i >= len(self.goals): return
 
-            goal = self.goals[self.i]
             self.redis_client.set(self.goal_keys[r], str(goal))
 
-            if self.pose_equals_goal(pose, goal):
-                self.i += 1
+            # without robots
+            # if r in self.goals:
+            #     goal = self.goals[r]
+            # else:
+            #     goal = Goal(x=float(r), y=0.0)
 
-    def pose_equals_goal(self, pose: Pose, goal: Goal):
-        if pose is None or goal is None:
-            return False
-        return (abs(pose.x - goal.x) < GOAL_TOLERANCE and 
-                abs(pose.y - goal.y) < GOAL_TOLERANCE)
+            # self.goals[r] = goal
+            # self.redis_client.set(self.goal_keys[r], str(goal))
+
+            # pose = Pose(x=float(goal.x), y=float(goal.y), h=0.0)
+
+            # self.poses[r] = goal
+            # self.redis_client.set(self.pose_keys[r], str(pose))
