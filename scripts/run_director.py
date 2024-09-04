5614afc691ea1ce25faead9d4ea19e2f17fcad3f
Michelle Pan <michellepan8@gmail.com>
2024-09-04 13:58:09 -0700

    Launch visualizer with director

--- scripts/run_director.py
+++ scripts/run_director.py
@@ -1,15 +1,30 @@
 import argparse
 import time
+from multiprocessing import Process
 
-from flocking.director import FlockPlanner
+from flocking.director import FlockPlanner, Visualizer
 
 
-def main(args=None, robots=(1,)):
-    director = FlockPlanner(robots=robots)
-    while True:
-        director.step_flocking()
-        time.sleep(0.1)
+def main(robots=(1,)):
 
+    def run_planner():
+        director = FlockPlanner(robots=robots)
+        while True:
+            director.step_flocking()
+            time.sleep(0.1) 
+
+    def run_visualizer():
+        visualizer = Visualizer(robots=robots)
+        visualizer.show_plot()
+        
+    p1 = Process(target=run_planner)
+    p1.start()
+
+    p2 = Process(target=run_visualizer)
+    p2.start()
+
+    p1.join()
+    p2.join()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
