5f49f4264d4730bcd0c9df7561d50743fade7283
Michelle Pan <michellepan8@gmail.com>
2024-09-04 17:05:20 -0700

    Update follower to read state from Redis

--- flocking/robot/flock_follower.py
+++ flocking/robot/flock_follower.py
@@ -19,12 +19,14 @@ GOAL_TOLERANCE = 0.1
 OBS_TOLERANCE = 0.5
 
 LIN_VEL_SCALE = 0.5
-ANG_VEL_SCALE = 1.0
+LIN_VEL_MAX = 0.3
 
-# MAX_ANG_SPEED = 1.0 
+ANG_VEL_SCALE = 1.0
+ANG_VEL_MAX = 1.0
 
 REDIS_HOST = "10.5.90.8"
 REDIS_PORT = "6379"
+
 
 class FlockFollower(Node):
 
@@ -36,74 +38,37 @@ class FlockFollower(Node):
         self.vel_pub = self.create_publisher(
             Twist, "/stretch/cmd_vel", 1)
 
-        # subscribers
-        self.scan_sub = self.create_subscription(
-            LaserScan, "/scan", self.scan_callback, 1)
-
         # redis
         self.redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT)
-        self.goal_timer = self.create_timer(0.1, self.redis_get_goal)
+        self.redis_timer = self.create_timer(0.01, self.read_redis)
 
         self.goal_key = robot_name + "::goal"
         self.pose_key = robot_name + "::pose"
+        self.obstacles_key = robot_name + "::obstacles"
 
         # initialize state
         self.pose = None
         self.goal = None
-        self.twist = Twist()
-
-        # tf
-        self.tf_buffer = Buffer()
-        self.tf_listener = TransformListener(self.tf_buffer, self)
-        self.tf_timer = self.create_timer(0.001, self.extract_pose)
-        self.extract_pose()
-
-        # clear old goal
-        self.redis_update_pose()
-        self.redis_client.set(self.goal_key, str(Goal(x=0.0, y=0.0)))
-  
         self.obstacle_present = False
 
-    ### POSE METHODS
+        # movement
+        self.move_timer = self.create_timer(0.01, self.move_toward_goal)
+        self.twist = Twist()
 
-    def print_pose(self):
+    def read_redis(self):
+        pose_str = self.redis_client.get(self.pose_key)
+        self.pose = Pose.from_string(pose_str)
+
+        goal_str = self.redis_client.get(self.goal_key)
+        self.goal = Goal.from_string(goal_str)
+
+        obstacle_str = self.redis_client.get(self.obstacles_key)
+        self.obstacle_present = obstacle_str == "True"
+
+        self.print_info()
+
+    def print_info(self):
         print(f"x: {self.pose.x : 5.2f}    y: {self.pose.y : 5.2f}    heading: {self.pose.h : 5.2f}     lin vel: {self.twist.linear.x : 5.2f}    ang vel: {self.twist.angular.z : 5.2f}")
-
-    def extract_pose(self):
-        to_frame = "map"
-        from_frame = "base_link"
-
-        try:
-            t = self.tf_buffer.lookup_transform(
-                    to_frame,
-                    from_frame,
-                    rclpy.time.Time())
-            # print(t)
-        except TransformException as ex:
-            self.get_logger().info(
-                f"Could not transform {to_frame} to {from_frame}: {ex}")
-            return
-
-        position = t.transform.translation
-        orientation = t.transform.rotation
-
-        quat = Rotation.from_quat([
-            orientation.x,
-            orientation.y,
-            orientation.z,
-            orientation.w])
-        euler = quat.as_euler("xyz") # radians
-
-        self.pose = Pose(
-            x=position.x,
-            y=position.y,
-            h=float(euler[2]))
-
-        self.redis_update_pose()
-        self.move_toward_goal()
-        self.print_pose()
-
-    ### GOAL METHODS
 
     def check_at_goal(self) -> bool:
         if self.pose is None or self.goal is None:
@@ -154,32 +119,3 @@ class FlockFollower(Node):
 
         # publish twist
         self.vel_pub.publish(self.twist)
-        # print("publishing")
-        # print(self.twist)
-
-    ### SUBSCRIBER CALLBACKS
-
-    def scan_callback(self, msg: LaserScan):
-        angles = np.linspace(msg.angle_min, msg.angle_max, len(msg.ranges))
-
-        # Work out the y coordinates of the ranges
-        points = [r * np.sin(theta) if (theta < -2.5 or theta > 2.5) else np.inf for r,theta in zip(msg.ranges, angles)]
-
-        # If we're close to the x axis, keep the range, otherwise use inf, which means "no return"
-        new_ranges = [r if abs(y) < 0.5 else np.inf for r,y in zip(msg.ranges, points)]
-
-        # If closest measured scan is within obstacle threshold, stop
-        self.obstacle_present = min(new_ranges) < OBS_TOLERANCE
-
-        if self.obstacle_present:
-            print("obstacle detected")
-
-    ### REDIS LISTENER
-
-    def redis_get_goal(self):
-        goal_str = self.redis_client.get(self.goal_key)
-        if goal_str:
-            self.goal = Goal.from_string(goal_str)
-
-    def redis_update_pose(self):
-        self.redis_client.set(self.pose_key, str(self.pose))
