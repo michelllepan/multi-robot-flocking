9aae8328366f8a8ab8f009e11abc50a41e328b3b
Michelle Pan <michellepan8@gmail.com>
2024-09-06 10:30:29 -0700

    Change process functions to top-module level

--- scripts/run_director.py
+++ scripts/run_director.py
@@ -5,22 +5,21 @@ from multiprocessing import Process
 from flocking.director import FlockPlanner, Visualizer
 
 
+def run_planner(robots):
+    director = FlockPlanner(robots=robots)
+    while True:
+        director.step_flocking()
+        time.sleep(0.1) 
+
+def run_visualizer(robots):
+    visualizer = Visualizer(robots=robots)
+    visualizer.show_plot()
+
 def main(robots=(1,)):
-
-    def run_planner():
-        director = FlockPlanner(robots=robots)
-        while True:
-            director.step_flocking()
-            time.sleep(0.1) 
-
-    def run_visualizer():
-        visualizer = Visualizer(robots=robots)
-        visualizer.show_plot()
-        
-    p1 = Process(target=run_planner)
+    p1 = Process(target=run_planner, args=(robots,))
     p1.start()
 
-    p2 = Process(target=run_visualizer)
+    p2 = Process(target=run_visualizer, args=(robots,))
     p2.start()
 
     p1.join()
