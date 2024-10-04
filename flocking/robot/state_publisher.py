5a35e4e1ce21aa8adfab5b46c3e52c9e9c638b2d
Michelle Pan <michellepan8@gmail.com>
2024-10-03 18:43:47 -0700

    Catch Redis exceptions

--- flocking/robot/state_publisher.py
+++ flocking/robot/state_publisher.py
@@ -1,4 +1,5 @@
 import os
+import time
 from io import BytesIO
 
 import numpy as np
@@ -74,57 +75,66 @@ class StatePublisher(Node):
         self.get_logger().info("created state publisher")
 
     def publish_pose(self):
-        to_frame, from_frame = "map", "base_link"
         try:
-            t = self.tf_buffer.lookup_transform(
-                    to_frame,
-                    from_frame,
-                    rclpy.time.Time())
-        except TransformException as ex:
-            self.get_logger().info(
-                f"Could not transform {to_frame} to {from_frame}: {ex}")
-            return
+            to_frame, from_frame = "map", "base_link"
+            try:
+                t = self.tf_buffer.lookup_transform(
+                        to_frame,
+                        from_frame,
+                        rclpy.time.Time())
+            except TransformException as ex:
+                self.get_logger().info(
+                    f"Could not transform {to_frame} to {from_frame}: {ex}")
+                return
 
-        position = t.transform.translation
-        orientation = t.transform.rotation
+            position = t.transform.translation
+            orientation = t.transform.rotation
 
-        quat = Rotation.from_quat([
-            orientation.x,
-            orientation.y,
-            orientation.z,
-            orientation.w])
-        euler = quat.as_euler("xyz") # radians
+            quat = Rotation.from_quat([
+                orientation.x,
+                orientation.y,
+                orientation.z,
+                orientation.w])
+            euler = quat.as_euler("xyz") # radians
 
-        self.pose = Pose(
-            x=position.x,
-            y=position.y,
-            h=float(euler[2]))
-        self.redis_client.set(self.pose_key, str(self.pose))
+            self.pose = Pose(
+                x=position.x,
+                y=position.y,
+                h=float(euler[2]))
+            self.redis_client.set(self.pose_key, str(self.pose))
+        except redis.exceptions.ConnectionError as e:
+            print(e, f" at time {time.time() : .0f}")
 
     def publish_battery(self, msg: BatteryState):
-        voltage = msg.voltage
-        self.redis_client.set(self.battery_key, voltage)
+        try:
+            voltage = msg.voltage
+            self.redis_client.set(self.battery_key, voltage)
+        except redis.exceptions.ConnectionError as e:
+            print(e, f" at time {time.time() : .0f}")
 
     def publish_obstacles(self, msg: LaserScan):
-        angles = np.linspace(msg.angle_min, msg.angle_max, len(msg.ranges))
+        try:
+            angles = np.linspace(msg.angle_min, msg.angle_max, len(msg.ranges))
 
-        ### FRONT
-        # work out the y coordinates of the ranges
-        points = [r * np.sin(theta) if (theta < -np.pi/2 or theta > np.pi/2) else np.inf for r,theta in zip(msg.ranges, angles)]
-        # if we're close to the x axis, keep the range, otherwise use inf, which means "no return"
-        new_ranges = [r if abs(y) < 0.5 else np.inf for r,y in zip(msg.ranges, points)]
-        # publish distance to closest point
-        self.redis_client.set(self.obstacles_front_key, str(min(new_ranges)))
+            ### FRONT
+            # work out the y coordinates of the ranges
+            points = [r * np.sin(theta) if (theta < -np.pi/2 or theta > np.pi/2) else np.inf for r,theta in zip(msg.ranges, angles)]
+            # if we're close to the x axis, keep the range, otherwise use inf, which means "no return"
+            new_ranges = [r if abs(y) < 0.5 else np.inf for r,y in zip(msg.ranges, points)]
+            # publish distance to closest point
+            self.redis_client.set(self.obstacles_front_key, str(min(new_ranges)))
 
-        ### BACK
-        points = [r * np.sin(theta) if (theta < 0.9 and theta > -0.9) else np.inf for r,theta in zip(msg.ranges, angles)]
-        new_ranges = [r if abs(y) < 0.5 else np.inf for r,y in zip(msg.ranges, points)]
-        self.redis_client.set(self.obstacles_back_key, str(min(new_ranges)))
+            ### BACK
+            points = [r * np.sin(theta) if (theta < 0.9 and theta > -0.9) else np.inf for r,theta in zip(msg.ranges, angles)]
+            new_ranges = [r if abs(y) < 0.5 else np.inf for r,y in zip(msg.ranges, points)]
+            self.redis_client.set(self.obstacles_back_key, str(min(new_ranges)))
 
-        ### SIDE
-        points = [r * np.cos(theta) if (theta > np.pi/4) else np.inf for r,theta in zip(msg.ranges, angles)]
-        new_ranges = [r if abs(x) < 0.5 else np.inf for r,x in zip(msg.ranges, points)]
-        self.redis_client.set(self.obstacles_side_key, str(min(new_ranges)))
+            ### SIDE
+            points = [r * np.cos(theta) if (theta > np.pi/4) else np.inf for r,theta in zip(msg.ranges, angles)]
+            new_ranges = [r if abs(x) < 0.5 else np.inf for r,x in zip(msg.ranges, points)]
+            self.redis_client.set(self.obstacles_side_key, str(min(new_ranges)))
+        except redis.exceptions.ConnectionError as e:
+            print(e, f" at time {time.time() : .0f}")
 
     # def publish_humans(self, detections_robot):
     #     if self.pose is None or self.head is None or detections_robot is None:
@@ -153,71 +163,83 @@ class StatePublisher(Node):
     #     self.redis_client.set(self.humans_key, str(detections_world))
 
     def publish_image(self):
-        depth_frame, color_frame = self.camera.get_frames()
-        depth_image, color_image = self.camera.convert_to_array(depth_frame, color_frame)
+        try:
+            depth_frame, color_frame = self.camera.get_frames()
+            depth_image, color_image = self.camera.convert_to_array(depth_frame, color_frame)
 
-        color_output = BytesIO()
-        color_image = color_image[:,:,::-1] # switch order of color channels
-        color_image = Image.fromarray(color_image.astype("uint8"), "RGB")
-        color_image.save(color_output, format="png")
-        self.redis_client.set(self.color_image_key, color_output.getvalue())
-        color_output.close()
+            color_output = BytesIO()
+            color_image = color_image[:,:,::-1] # switch order of color channels
+            color_image = Image.fromarray(color_image.astype("uint8"), "RGB")
+            color_image.save(color_output, format="png")
+            self.redis_client.set(self.color_image_key, color_output.getvalue())
+            color_output.close()
 
-        depth_output = BytesIO()
-        np.save(depth_output, depth_image, allow_pickle=False)
-        self.redis_client.set(self.depth_image_key, depth_output.getvalue())
-        depth_output.close()
+            depth_output = BytesIO()
+            np.save(depth_output, depth_image, allow_pickle=False)
+            self.redis_client.set(self.depth_image_key, depth_output.getvalue())
+            depth_output.close()
+        except redis.exceptions.ConnectionError as e:
+            print(e, f" at time {time.time() : .0f}")
 
     def publish_head(self, msg: JointState):
-        index = msg.name.index("joint_head_pan")
-        value = msg.position[index]
-        self.head = value
-        self.redis_client.set(self.head_key, self.head)
+        try:
+            index = msg.name.index("joint_head_pan")
+            value = msg.position[index]
+            self.head = value
+            self.redis_client.set(self.head_key, self.head)
+        except redis.exceptions.ConnectionError as e:
+            print(e, f" at time {time.time() : .0f}")
 
     def publish_music_base(self, msg: Twist):
-        linear = msg.linear.x
-        if abs(linear) > 1e-3:
-            self.redis_client.set(self.music_key_prefix + "0", "play")
-        else:
-            self.redis_client.set(self.music_key_prefix + "0", "stop")
+        try:
+            linear = msg.linear.x
+            if abs(linear) > 1e-3:
+                self.redis_client.set(self.music_key_prefix + "0", "play")
+            else:
+                self.redis_client.set(self.music_key_prefix + "0", "stop")
 
-        angular = msg.angular.z
-        if abs(angular) > 1e-3:
-            self.redis_client.set(self.music_key_prefix + "1", "play")
-        else:
-            self.redis_client.set(self.music_key_prefix + "1", "stop")
+            angular = msg.angular.z
+            if abs(angular) > 1e-3:
+                self.redis_client.set(self.music_key_prefix + "1", "play")
+            else:
+                self.redis_client.set(self.music_key_prefix + "1", "stop")
+        except redis.exceptions.ConnectionError as e:
+            print(e, f" at time {time.time() : .0f}")
 
     def publish_music_joints(self, msg: JointState):
-        if "joint_lift" in msg.name:
-            lift_index = msg.name.index("joint_lift")
-            lift_vel = msg.velocity[lift_index]
-            if abs(lift_vel) > 1e-3:
-                self.redis_client.set(self.music_key_prefix + "2", "play")
-            else:
-                self.redis_client.set(self.music_key_prefix + "2", "stop")
+        try:
+            if "joint_lift" in msg.name:
+                lift_index = msg.name.index("joint_lift")
+                lift_vel = msg.velocity[lift_index]
+                if abs(lift_vel) > 1e-3:
+                    self.redis_client.set(self.music_key_prefix + "2", "play")
+                else:
+                    self.redis_client.set(self.music_key_prefix + "2", "stop")
 
-        if "joint_arm_l3" in msg.name:
-            arm_index = msg.name.index("joint_arm_l3")
-            arm_vel = msg.velocity[arm_index]
-            if abs(arm_vel) > 1e-3:
-                self.redis_client.set(self.music_key_prefix + "3", "play")
-            else:
-                self.redis_client.set(self.music_key_prefix + "3", "stop")
+            if "joint_arm_l3" in msg.name:
+                arm_index = msg.name.index("joint_arm_l3")
+                arm_vel = msg.velocity[arm_index]
+                if abs(arm_vel) > 1e-3:
+                    self.redis_client.set(self.music_key_prefix + "3", "play")
+                else:
+                    self.redis_client.set(self.music_key_prefix + "3", "stop")
 
-        if "joint_wrist_yaw" in msg.name:
-            wrist_yaw_index = msg.name.index("joint_wrist_yaw")
-            wrist_yaw_vel = msg.velocity[wrist_yaw_index]
-            if abs(wrist_yaw_vel) > 1e-3:
-                self.redis_client.set(self.music_key_prefix + "4", "play")
-            else:
-                self.redis_client.set(self.music_key_prefix + "4", "stop")
+            if "joint_wrist_yaw" in msg.name:
+                wrist_yaw_index = msg.name.index("joint_wrist_yaw")
+                wrist_yaw_vel = msg.velocity[wrist_yaw_index]
+                if abs(wrist_yaw_vel) > 1e-3:
+                    self.redis_client.set(self.music_key_prefix + "4", "play")
+                else:
+                    self.redis_client.set(self.music_key_prefix + "4", "stop")
 
-        if "joint_wrist_pitch" in msg.name:
-            wrist_pitch_index = msg.name.index("joint_wrist_pitch")
-            wrist_pitch_vel = msg.velocity[wrist_pitch_index]
-            if abs(wrist_pitch_vel) > 1e-3:
-                self.redis_client.set(self.music_key_prefix + "5", "play")
-            else:
-                self.redis_client.set(self.music_key_prefix + "5", "stop")
+            if "joint_wrist_pitch" in msg.name:
+                wrist_pitch_index = msg.name.index("joint_wrist_pitch")
+                wrist_pitch_vel = msg.velocity[wrist_pitch_index]
+                if abs(wrist_pitch_vel) > 1e-3:
+                    self.redis_client.set(self.music_key_prefix + "5", "play")
+                else:
+                    self.redis_client.set(self.music_key_prefix + "5", "stop")
+        except redis.exceptions.ConnectionError as e:
+            print(e, f" at time {time.time() : .0f}")
 
-        
+            
